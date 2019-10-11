7c4ce43da8df4046be035ead46adb72d56e4c030
Lars Montanaro <lmontanaro@flashpoint-intel.com>
2019-10-11 19:38:35 -0400

    customized an image stepper

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import AppBar from '@material-ui/core/AppBar';
+import Grid from '@material-ui/core/Grid';
+import ImageStepper from './components/ImageStepper.js';
 import './App.css';
+
+const mockSteps = [
+  {
+    label: 'Bird',
+    imgPath:
+      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+  {
+    label: 'Bird',
+    imgPath:
+      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+  {
+    label: 'Bali, Indonesia',
+    imgPath:
+      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
+  },
+  {
+    label: 'NeONBRAND Digital Marketing, Las Vegas, United States',
+    imgPath:
+      'https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+  {
+    label: 'Goč, Serbia',
+    imgPath:
+      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar />
+      <Grid>
+        <ImageStepper items={mockSteps}/>
+      </Grid>
     </div>
   );
 }
---
+++ src/components/ImageStepper.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    maxWidth: 500,
+    flexGrow: 1,
+  },
+  header: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 20,
+    padding: 8,
+    backgroundColor: theme.palette.background.default,
+  },
+  img: {
+    maxHeight: 318,
+    display: 'block',
+    maxWidth: 500,
+    overflow: 'hidden',
+    width: '100%',
+  },
+  stepper: {
+    height: 20,
+    padding: 8,
+  },
+}));
+
+function ImageStepper(props) {
+  const classes = useStyles();
+  const { items, autoStep, stepInterval } = props;
+
+  const View = autoStep ? autoPlay(SwipeableViews) : SwipeableViews;
+
+  const maxSteps = items.length;
+  const [activeStep, setActiveStep] = React.useState(0);
+  const handleNext = () => setActiveStep(prevActiveStep => prevActiveStep + 1);
+  const handleBack = () => setActiveStep(prevActiveStep => prevActiveStep - 1);
+  const handleStepChange = step => setActiveStep(step);
+
+  return (
+    <div className={classes.root}>
+      <Card elevation={0}> 
+        <div className={classes.header}>
+          <Typography>{items[activeStep].label}</Typography>
+        </div>
+        <View
+          axis={'x'}
+          index={activeStep}
+          interval={autoStep ? stepInterval : null}
+          onChangeIndex={handleStepChange}
+          enableMouseEvents
+        >
+          {items.map((item, index) => (
+            <div key={item.imgPath}>
+              {Math.abs(activeStep - index) <= 2 ? (
+                <img className={classes.img} src={item.imgPath} alt={item.label} />
+              ) : null}
+            </div>
+          ))}
+        </View>
+        <MobileStepper
+          className={classes.stepper}
+          position="block"
+          variant="dots"
+          activeStep={activeStep}
+          steps={maxSteps}
+          nextButton={
+            <Button disableRipple size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+              {<KeyboardArrowRight />}
+            </Button>
+          }
+          backButton={
+            <Button disableRipple size="small" onClick={handleBack} disabled={activeStep === 0}>
+              {<KeyboardArrowLeft />}
+            </Button>
+          }
+        />
+      </Card>
+    </div>
+  );
+}
+
+ImageStepper.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.shape({
+    imgPath: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+  })).isRequired,
+  autoStep: PropTypes.bool,
+  stepInterval: PropTypes.number,
+}
+
+ImageStepper.propTypes = {
+  autoStep: false,
+  stepInterval: 3000,
+}
+
+export default ImageStepper;
