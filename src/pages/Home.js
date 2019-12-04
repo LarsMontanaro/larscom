2c2b03accdb003e609f65decfa6cd214104a5560
Lars Montanaro <lmontanaro@flashpoint-intel.com>
2019-12-04 11:08:39 -0500

    more small changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,7 +20,7 @@ const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
     justifyContent: 'center',
-    '& h1, h2':  {
+    '& h1, h2, h3, h4':  {
       margin: 0,
     },
   },
@@ -62,35 +62,52 @@ const Home = () => {
         <Header />
         <Grid container spacing={2}>
           <Grid item xs={6}>
-            {/* Col 1 */}
-            {/* BIO */}
-            <h1>about</h1>
-            <span>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</span>
-            {/* Headshot and resume */}
-            <h1>headshot & resume</h1>
-            <ImageCard img={headshotImg} download={{ file: headshotImg, fileName: "headshot_LarsMontanaro"}} />
-            <ImageCard img={resumeImg} download={{ file: resume, fileName: "resume_LarsMontanaro"}} />
-            <h1>say hi!</h1>
-            <div className={classes.contactIconsContainer}>
-              <a href="mailto:larsmontanaro@gmail.com">
-                <IconButton>
-                  {<Email />}
-                </IconButton>
-              </a>
-              <SocialIcon target="_blank" url="https://www.instagram.com/larsmontanaro/" />
-              <SocialIcon target="_blank" url="https://twitter.com/lars_montanaro" />
-            </div>
+            <Grid item>
+              <h1>about</h1>
+              <span>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</span>
+            </Grid>
+            <Grid item>
+              <h1>headshot & resume</h1>
+              <ImageCard img={headshotImg} download={{ file: headshotImg, fileName: "headshot_LarsMontanaro"}} />
+              <ImageCard img={resumeImg} download={{ file: resume, fileName: "resume_LarsMontanaro"}} />
+            </Grid>
+            <Grid item>
+              <h1>say hi!</h1>
+              <div className={classes.contactIconsContainer}>
+                <a href="mailto:larsmontanaro@gmail.com">
+                  <IconButton>
+                    {<Email />}
+                  </IconButton>
+                </a>
+                <SocialIcon target="_blank" url="https://www.instagram.com/larsmontanaro/" />
+                <SocialIcon target="_blank" url="https://twitter.com/lars_montanaro" />
+              </div>
+            </Grid>
           </Grid>
           <Grid item xs={6}>
-            <h1>news</h1>
-            <div className={classes.stepperTitleText}>
-              <h2>Animamundi</h2>
-              <span>Ollie Mers</span>
-            </div>
-            <ImageStepper items={animaSteps}/>
-            <ImageStepper items={dcaSteps}/>
-            <ImageStepper items={roomSteps}/>
-            <ImageStepper items={dogsSteps}/>
+            <Grid item>
+              <h1>news</h1>
+              <div className={classes.stepperTitleText}>
+                <h3>Animamundi</h3>
+                <span>&nbsp;-&nbsp;Ollie Mers</span>
+              </div>
+              <ImageStepper items={animaSteps}/>
+              <div className={classes.stepperTitleText}>
+                <h3>DCA</h3>
+                <span>&nbsp;-&nbsp;NYU Dancers & Choreographers Alliance</span>
+              </div>
+              <ImageStepper items={dcaSteps}/>
+              <div className={classes.stepperTitleText}>
+                <h3>A Room For Lost Things</h3>
+                <span>&nbsp;-&nbsp;Ana Schuler</span>
+              </div>
+              <ImageStepper items={roomSteps}/>
+              <div className={classes.stepperTitleText}>
+                <h3>Dogs Chase Shadows</h3>
+                <span>&nbsp;-&nbsp;Malena Pennycook</span>
+              </div>
+              <ImageStepper items={dogsSteps}/>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
