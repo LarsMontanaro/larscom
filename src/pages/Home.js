54f6573a0f95cc6b8d562e458f4bdf4739e93094
Lars Montanaro <lmontanaro@flashpoint-intel.com>
2020-10-20 13:42:19 -0700

    added some stuff

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,6 +10,7 @@ import animaImgs from '../img/anima';
 import dcaImgs from '../img/dca';
 import dogsImgs from '../img/dogs';
 import roomImgs from '../img/room';
+import labImgs from '../img/lab';
 import headshotImg from '../img/headshot_LarsMontanaro.jpg';
 import resumeImg from '../img/resume_LarsMontanaro.jpg';
 import resume from '../img/resume_LarsMontanaro.pdf';
@@ -28,9 +29,14 @@ const useStyles = makeStyles(theme => ({
     width: '80%',
   },
   colsWrapper: {
+    marginTop: 12,
     '& > *': {
       maxWidth: 700,
     },
+  },
+  colScroll: {
+    maxHeight: 2000,
+    overflow: 'scroll',
   },
   contactIconsContainer: {
     display: 'flex',
@@ -53,13 +59,14 @@ const Home = () => {
   const dcaSteps = dcaImgs.map((img) => ({ img }));
   const dogsSteps = dogsImgs.map((img) => ({ img }));
   const roomSteps = roomImgs.map((img) => ({ img }));
+  const labSteps = labImgs.map((img) => ({ img }));
 
   return (
     <div className={classes.root}>
       <Grid container className={classes.pageContainer} spacing={2}>
         <Header />
         <Grid container spacing={4} className={classes.colsWrapper}>
-          <Grid item xs={12} md={6}>
+          <Grid item xs={12} md={6} className={classes.col1}>
             <Grid item className={classes.about}>
               <h1>about</h1>
               <p>Lars is a Brooklyn-based actor/comedian/theater-maker.</p>
@@ -106,114 +113,133 @@ const Home = () => {
               </div>
             </Grid>
           </Grid>
-          <Grid item xs={12} md={6}>
+          <Grid item xs={12} md={6} className={classes.col2}>
             <Grid item className={classes.news}>
               <h1>news</h1>
-              <div>
-                <p>
-                  <b>+</b> Lars joins the&nbsp;
-                  <a href="http://exubs.com/">The Exuberants Improv Comedy Troupe</a>
-                  and will perform Sunday nighst at 6pm at&nbsp;
-                  <a href="https://www.broadwaycomedyclub.com/">Broadway Comedy Club!</a>
-                </p>
-              </div>
-              <div>
-                <p>
-                  <b>+</b> Lars will be workshopping a new play with the&nbsp;
-                  <a href="https://www.hudsonshakespeare.com/">Hudson Shakespeare Company</a>
-                  ! details to come!
-                </p>
-              </div>
-              <div>
-                <p>
-                  <b>+</b> An absolute blast has been had playing with the clowns at the&nbsp;
-                  <a href="https://www.facebook.com/NYC-Physical-Comedy-Lab-1398973467082552">
-                  NYC Physical Comedy Lab
-                  </a>
-                  &nbsp;each week! :-)
-                </p>
-              </div>
-              <div>
-                <p>
-                  <b>+</b> Lars delves into text and physical impulse in David Prete's&nbsp;
-                  <a href="https://www.davidprete.com/the-plastiques-project">Plastiques Project</a>.
-                </p>
-              </div>
-              <div>
-                <p>
-                  <b>+</b> Lars has some serious fun at Avner Eisenberg's 'Eccentric
-                  Performance' clowning workshop at the&nbsp;
-                  <a href="https://celebrationbarn.com/">
-                  Celebration Barn Theater
-                  </a>
-                  &nbsp;!
-                </p>
-              </div>
-              <div>
-                <p>
-                  <b>+</b> the Theater of the Oppressed Facilitator Training at the&nbsp;
-                  <a href="http://www.mandalaforchange.com/">
-                  Mandala Center for Change
-                  </a>
-                  &nbsp;with Marc Weinblatt was an intense and illuminating experience.
-                </p>
-              </div>
-              <div>
-                <p>
-                  <b>+</b> a plastic-sheet sea, a cardboard-box rainforest, and a
-                  river of trash tranform the stage in Ollie Mers' <i>Animamundi</i>,
-                  at NYU's Experimental Theatre Wing. Lars devises and plays in a
-                  menagerie of vignettes that explore what it might be like to
-                  be non-human in a swiftly transforming environment.
-                </p>
-                <span><i>Animamundi</i></span>
-                <ImageStepper items={animaSteps} />
-              </div>
-              <div>
-                <p>
-                  <b>+</b> Lars is thrilled to be training under Erica Fae in a spring
-                  Grotowski workshop produced by&nbsp;
-                  <a href="http://www.synaesthetic-theatre.com/">Synaesthetic Theater</a>.
-                </p>
-              </div>
-              <div>
-                <p>
-                  <b>+</b> Lars appears in Eleanor Maples' production of <i>Peter and
-                  the Wolf</i>. This movement-focused children's tale went up at
-                  NYU's Playwrights Horizons Studio.
-                </p>
-              </div>
-              <div>
-                <p>
-                  <b>+</b> Lars performs in his final showcase with the NYU Dancers
-                  & Choreographers Alliance. Will miss you, DCA.
-                </p>
-                <span><i>DCA</i></span>
-                <ImageStepper items={dcaSteps}/>
-              </div>
-              <div>
-                <p>
-                  <b>+</b> Lars performs his last show with the indubitable Nut Mix improv
-                  at the Holbrooke Hotel in his beloved hometown of Nevada City, CA.
-                </p>
-              </div>
-              <div>
-                <p>
-                  <b>+</b> Lars leads pattering puppet feet up and down a
-                  stairway in the clouds in Ana Schuler's <i>A Room For
-                  Lost Things</i> at NYU Experimental Theatre Wing.
-                </p>
-                <span><i>A Room For Lost Things</i></span>
-                <ImageStepper items={roomSteps} />
-              </div>
-              <div>
-                <p>
-                  <b>+</b>Lars is an actor, meta-actor, dancer, silhouettist,
-                  and tree (dream part!) in Malena Pennycooks's <i>Dogs Chase
-                  Shadows</i> at NYU's Experimental Theater Wing.
-                </p>
-                <span><i>Dogs Chase Shadows</i></span>
-                <ImageStepper items={dogsSteps} />
+              <div className={classes.colScroll}>
+                <div>
+                  <p>
+                    <b>+</b> &nbsp;
+                    Lars will be joining
+                    &nbsp;<a href="https://www.marinshakespeare.org/">Marin Shakespeare Company</a>
+                    &nbsp;as an acting apprentice for their fall season.
+                    <span style={{ fontStyle: "italic" }}> *deferred due to COVID =(</span>
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    <b>+</b> &nbsp;
+                    <a href="https://www.irteinfo.com/">IRTE</a>
+                    , the Improvisational Repertory Theatre Ensemble, welcomes Lars for&nbsp;
+                    <a href="https://www.irteinfo.com/shows">Diner on the Edge!</a>
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    <b>+</b> Lars joins&nbsp;
+                    <a href="http://exubs.com/">The Exuberants Improv Comedy Troupe</a>
+                    &nbsp;and performs every Sunday night @ 6pm at the&nbsp;
+                    <a href="https://www.broadwaycomedyclub.com/">Broadway Comedy Club!</a>
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    <b>+</b> Lars will be workshopping a new play with the&nbsp;
+                    <a href="https://www.hudsonshakespeare.com/">Hudson Shakespeare Company</a>
+                    ! details to come!
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    <b>+</b> An absolute blast has been had playing with the clowns at the&nbsp;
+                    <a href="https://www.facebook.com/NYC-Physical-Comedy-Lab-1398973467082552">
+                    NYC Physical Comedy Lab
+                    </a>
+                    &nbsp;each week! :-)
+                  </p>
+                  <ImageStepper items={labSteps}/>
+                </div>
+                <div>
+                  <p>
+                    <b>+</b> Lars delves into text and physical impulse in David Prete's&nbsp;
+                    <a href="https://www.davidprete.com/the-plastiques-project">Plastiques Project</a>.
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    <b>+</b> Lars has some serious fun at Avner Eisenberg's 'Eccentric
+                    Performance' clowning workshop at the&nbsp;
+                    <a href="https://celebrationbarn.com/">
+                    Celebration Barn Theater
+                    </a>
+                    &nbsp;!
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    <b>+</b> the Theater of the Oppressed Facilitator Training at the&nbsp;
+                    <a href="http://www.mandalaforchange.com/">
+                    Mandala Center for Change
+                    </a>
+                    &nbsp;with Marc Weinblatt was an intense and illuminating experience.
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    <b>+</b> a plastic-sheet sea, a cardboard-box rainforest, and a
+                    river of trash tranform the stage in Ollie Mers' <i>Animamundi</i>.
+                    Lars devises and plays in a menagerie of vignettes that explore what 
+                    it might be like to be non-human in a swiftly transforming environment.
+                  </p>
+                  <span><i>Animamundi</i></span>
+                  <ImageStepper items={animaSteps} />
+                </div>
+                <div>
+                  <p>
+                    <b>+</b> Lars is thrilled to be training under Erica Fae in a spring
+                    Grotowski workshop produced by&nbsp;
+                    <a href="http://www.synaesthetic-theatre.com/">Synaesthetic Theater</a>.
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    <b>+</b> Lars appears in Eleanor Maples' production of <i>Peter and
+                    the Wolf</i>. This movement-focused children's tale went up at
+                    NYU's Playwrights Horizons Studio.
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    <b>+</b> Lars performs in his final showcase with the NYU Dancers
+                    & Choreographers Alliance. Will miss you, DCA.
+                  </p>
+                  <span><i>DCA</i></span>
+                  <ImageStepper items={dcaSteps}/>
+                </div>
+                <div>
+                  <p>
+                    <b>+</b> Lars performs his last show with the indubitable Nut Mix improv
+                    at the Holbrooke Hotel in his beloved hometown of Nevada City, CA.
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    <b>+</b> Lars leads pattering puppet feet up and down a
+                    stairway in the clouds in Ana Schuler's <i>A Room For
+                    Lost Things</i> at NYU Experimental Theatre Wing.
+                  </p>
+                  <span><i>A Room For Lost Things</i></span>
+                  <ImageStepper items={roomSteps} />
+                </div>
+                <div>
+                  <p>
+                    <b>+</b>Lars is an actor, meta-actor, dancer, silhouettist,
+                    and tree (dream part!) in Malena Pennycooks's <i>Dogs Chase
+                    Shadows</i> at NYU's Experimental Theater Wing.
+                  </p>
+                  <span><i>Dogs Chase Shadows</i></span>
+                  <ImageStepper items={dogsSteps} />
+                </div>
               </div>
             </Grid>
           </Grid>
